fbbe0fec8e94f48ab2b30dff5ca7a672b1d58973
Taha-Hasemi <kiziltahaaslan@gmail.com>
2025-02-24 16:57:03 +0330

    feat: security and validation aspect

--- src/aspects/securityAspect.ts
+++ src/aspects/securityAspect.ts
@@ -1,13 +1,17 @@
 import { ExecutionContext, CallHandler } from "@nestjs/common";
 import Aspect from "src/utils/aspects/aspect";
 
-export class ExceptionAspcet extends Aspect {
+export default class SecurityAspect extends Aspect {
+    constructor(private readonly claims: string[]) { super() }
     onBefore(context: ExecutionContext, next: CallHandler<any>): void {
+        // if (this.claims.includes(context.switchToHttp().getRequest().headers['authorization'])) {
+
+        // } else
+            // throw 'sd';
     }
     onAfter(context: ExecutionContext, next: CallHandler<any>): void {
     }
     onException(error: any, context: ExecutionContext, next: CallHandler<any>): void {
-        console.log('on exception: ' + JSON.stringify(error))
     }
     onSuccess(context: ExecutionContext, next: CallHandler<any>): void {
     }
